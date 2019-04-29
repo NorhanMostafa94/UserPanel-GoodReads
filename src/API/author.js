ab289c99ffa1b8ab34baa239048ae9f41a67aa9f
DoaaOsama <doaa_kandeel@hotmail.com>
2019-04-29 11:14:23 +0200

    add process enviroment backendurl

--- src/API/author.js
+++ src/API/author.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-const BACKEND_URL = 'http://localhost:3000';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
 
 export const getAuthors = () => {
     return axios.get(`${BACKEND_URL}/api/authors`)
         .then(res => res.data)
 }
 
-export const getAuthorById = async (authorId)=>{
+export const getAuthorById = async (authorId) => {
     const author = await axios.get(`${BACKEND_URL}/api/authors/${authorId}`)
     // const books = author.data.books;
     return author.data;
--- src/API/category.js
+++ src/API/category.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'http://localhost:3000';
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
 
 export const getCategories = () => {
     return axios.get(`${BACKEND_URL}/api/categories`)
         .then(res => res.data)
 }
 
-export const getCategoryById = async(categoryId)=>{
+export const getCategoryById = async (categoryId) => {
     const categories = await axios.get(`${BACKEND_URL}/api/categories/${categoryId}`)
     // const books = categories.data.books
     return categories.data;
